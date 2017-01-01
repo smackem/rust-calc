14964c11d48ec911c47faa92d11b30918bf868fe
Smackem <sm4ckem@outlook.com>
2017-01-01 18:35:21 +0100

    finished first working version with repl

--- rcalc/src/main.rs
+++ rcalc/src/main.rs
@@ -6,20 +6,44 @@ mod parser;
 mod interpreter;
 
 use std::collections::HashMap;
+use std::io;
+use std::io::Write;
 
 fn main() {
-    let input = lexer::lex("1 + 2 * (3 - 2) + a - b");
-    println!("Tokens: {:?}", input);
-
-    let expr = parser::parse(&input);
-    println!("Ast: {:?}", expr);
-
     let ctx = {
         let mut map: HashMap<String, i32> = HashMap::new();
         map.insert("a".to_string(), 1);
         map.insert("b".to_string(), 2);
         interpreter::context_from_hashmap(map)
     };
-    let res = interpreter::interpret(&expr, &*ctx);
-    println!("Result: {}", res);
+
+    loop {
+        print!("> ");
+        io::stdout().flush().expect("");
+
+        let line = {
+            let mut buf = String::new();
+            io::stdin().read_line(&mut buf).expect("stdin failure");
+            buf
+        };
+
+        match line.trim() {
+            "#q" => break,
+            "" => (),
+            line => {
+                let res = eval(line, &*ctx);
+                println!("= {}", res);
+            },
+        }
+    }
+}
+
+fn eval(line: &str, ctx: &interpreter::Context) -> i32 {
+    let input = lexer::lex(&line);
+    println!("Tokens: {:?}", input);
+
+    let expr = parser::parse(&input);
+    println!("Ast: {:?}", expr);
+
+    interpreter::interpret(&expr, ctx)
 }

51765455a093ef130fb68e63fb3f8601545c3848
smackem <sm4ckem@outlook.com>
2017-02-24 15:56:10 +0100

    resolved #15 (JSON file output)

--- rcalc/src/main.rs
+++ rcalc/src/main.rs
@@ -2,6 +2,9 @@ extern crate rcalc;
 
 use std::io;
 use std::io::{ BufWriter, Write };
+use std::path::PathBuf;
+use std::fs::File;
+use std::env;
 use rcalc::{ Calculator, RuntimeItem, Value };
 
 fn main() {
@@ -37,11 +40,18 @@ fn main() {
                 };
                 println!("{}", String::from_utf8(buf).unwrap());
             },
+            "#w" => {
+                match write_json(&calculator) {
+                    Result::Ok(ref path) => println!("Context written to '{}'", path),
+                    Result::Err((ref path, ref error)) => println!("Error writing context to '{}': {:?}", path, error),
+                };
+            },
             "?" => {
                 println!("#q - quit rcalc");
                 println!("#: - enter or leave parallel mode");
                 println!("#! - leave parallel mode and evaluate all input concurrently");
                 println!("#j - print a JSON representation of the current values");
+                println!("#w - write a JSON representation of the current values to a file");
             },
             line if line.starts_with("#precision ") => {
                 if let Result::Ok(p) = line.split_whitespace().last().unwrap().parse::<usize>() {
@@ -98,4 +108,25 @@ fn print_value(v: &Value, precision: &Option<usize>) {
             }
         },
     }
+}
+
+fn write_json(calculator: &Calculator) -> Result<String, (String, ::std::io::Error)> {
+    fn inner_write(calculator: &Calculator, path: &str) -> ::std::io::Result<()> {
+        let mut writer = BufWriter::new(try!(File::create(&path)));
+        calculator.write_json(&mut writer)
+    }
+    let path = get_json_path();
+    match inner_write(calculator, &path) {
+        Result::Ok(()) => Result::Ok(path),
+        Result::Err(error) => Result::Err((path, error)),
+    }
+}
+
+fn get_json_path() -> String {
+    let mut pathbuf: PathBuf = match env::home_dir() {
+        Some(path) => PathBuf::from(path),
+        None => PathBuf::from(""),
+    };
+    pathbuf.push("rcalc.json");
+    pathbuf.to_str().unwrap().to_string()
 }
